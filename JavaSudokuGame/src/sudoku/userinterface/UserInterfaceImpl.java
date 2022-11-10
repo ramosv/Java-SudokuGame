e936296127320657e814f246214553fe9e9d8657
ramosv <56368263+ramosv@users.noreply.github.com>
2022-11-09 23:54:57 -0700

    Progress ...

--- JavaSudokuGame/src/sudoku/userinterface/UserInterfaceImpl.java
+++ JavaSudokuGame/src/sudoku/userinterface/UserInterfaceImpl.java
@@ -3,8 +3,12 @@ package sudoku.userinterface;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import sudoku.problemdomain.Coordinates;
 import sudoku.problemdomain.SudokuGame;
+
+import java.util.HashMap;
 
 public class UserInterfaceImpl implements IUserInterfaceContract.View,
         EventHandler<KeyEvent> {
@@ -12,15 +16,39 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View,
     private final Stage stage;
     private final Group root;
 
+    private HashMap<Coordinates, SudokuTextField> textFieldCoordinates;
 
-    @Override
-    public void handle(KeyEvent keyEvent) {
+    private IUserInterfaceContract.EventListener listener;
+
+    private static final double WINDOW_Y = 732;
+    private static final double WINDOW_X = 668;
+    private static final double BOARD_PADDING = 50;
+    private static final double BOARD_X_AND_y = 576;
+
+    private static final Color WINDOW_BACKGROUND_COLOR = Color.rgb(0, 150, 136);
+    private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(224, 242, 241);
+    private static final String SUDOKU = "Sudoku";
+
+    public UserInterfaceImpl(Stage stage) {
+        this.stage = stage;
+        this.root = new Group();
+        this.textFieldCoordinates = new HashMap<>();
+        initializeUserInterface();
+    }
+
+    private void initializeUserInterface() {
+        drawBackground(root);
+        drawTitle(root);
+        drawSudokuBoard(root);
+        drawTextFields(root);
+        drawGridLines(root);
+        stage.show();
 
     }
 
     @Override
     public void setListener(IUserInterfaceContract.EventListener listener) {
-
+        this.listener = listener;
     }
 
     @Override
@@ -40,6 +68,11 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View,
 
     @Override
     public void showError(String message) {
+
+    }
+
+    @Override
+    public void handle(KeyEvent keyEvent) {
 
     }
 }
